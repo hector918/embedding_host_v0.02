a3aa043acf24c22e827d2e1fd2a10d6a8d679754
hector918 <hector_zhong@outlook.com>
2024-09-03 09:05:39 -0400

    read news from db finished

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from psycopg2 import sql
+from datetime import datetime, timedelta
+from queries.news_table import read_news_rows, read_news_row_by_hashlist
+
+#example by default query
+ret = read_news_rows()
+print(ret)
+
+#can also customize either columns, number of returning rows and time period,
+ret = read_news_rows(columns=['hash_code', 'title'], limit=2, period={"start_date": datetime.now() - timedelta(days=365), "end_date": datetime.now()})
+print(ret)
 
 
-from variable_ import api_key
-
-print(api_key)
+#query by hash code, for constant output
+ret = read_news_row_by_hashlist(hash_list=["c1eede8b8f528caaacda3ef3b97d5d121f1d29fe9c3de99dc5649aa16e01357e"])
+print(ret)
--- variable_.py
+++ variable_.py
@@ -1,18 +1,20 @@
 import os
-
+#
+env_config = {}
 def load_env_file(filepath):
   with open(filepath, 'r') as file:
     for line in file:
-        # 忽略注释和空行
-        if line.startswith('#') or not line.strip():
-          continue
-        # 拆分键和值，并去掉左右的空白字符
-        key, value = line.strip().split('=', 1)
-        os.environ[key] = value
-
+      # 忽略注释和空行
+      if line.startswith('#') or not line.strip():
+        continue
+      # 拆分键和值，并去掉左右的空白字符
+      key, value = line.strip().split('=', 1)
+      env_config[key] = value
+#
 # 指定 .env 文件的路径
 env_file_path = '.env'
 load_env_file(env_file_path)
 
-# 访问环境变量
-api_key = os.getenv('DEBUG')
+root_folder = os.getcwd()
+rss_news_table_name_prefix = "z_rss_t_"
+
